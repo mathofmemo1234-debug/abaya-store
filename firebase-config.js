fb06961e4aab4f482037501fde9e48859412de86
Mohamed Jumaa <mathofmemo1234@gmail.com>
2026-08-12 07:25:50 +0300

    Update firebase config template with new project ID and database URL

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 // يرجى استبدال هذه الإعدادات ببيانات مشروع Firebase الجديد الخاص بك من لوحة تحكم Firebase Console
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID_HERE.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID_HERE-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_PROJECT_ID_HERE.firebasestorage.app",
+  authDomain: "abaya-store-b8841.firebaseapp.com",
+  databaseURL: "https://abaya-store-b8841-default-rtdb.firebaseio.com",
+  projectId: "abaya-store-b8841",
+  storageBucket: "abaya-store-b8841.firebasestorage.app",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
   appId: "YOUR_APP_ID_HERE"
 };
